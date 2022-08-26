834fe7256da960d2ec0cba774f2f8760cb31afd2
haowang <593301179@qq.com>
2022-08-26 10:01:58 +0800

    enum add order test

--- src/BasicDemo/Enums/EnumTest.cs
+++ src/BasicDemo/Enums/EnumTest.cs
@@ -13,7 +13,7 @@ internal class EnumTest
     internal static void TestUnkownValue()
     {
         Gender gender = Gender.Unknow;
-        if(Enum.TryParse("UnknowValue",out Gender result))
+        if (Enum.TryParse("UnknowValue", out Gender result))
         {
             gender = result;
         }
@@ -28,8 +28,48 @@ internal class EnumTest
         Console.WriteLine(enumDefaultValue.Gender);
     }
 
-    public class EnumDefaultValue
+    [Command("enum-test-order")]
+    internal static void TestOrder()
+    {
+        var list = new List<EnumOrderClass>();
+        for (int i = 0; i < 10; i++)
+        {
+            var value = Random.Shared.Next(1, 100) % 4;
+            Gender gender = (Gender)value;
+            list.Add(new EnumOrderClass(gender, value, value.ToString()));
+        }
+
+        list.Add(new EnumOrderClass((Gender)4, 4, "4"));
+
+        foreach (var item in list.OrderBy(l => l.Gender))
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+
+    internal class EnumDefaultValue
     {
         public Gender Gender { get; set; }
+    }
+
+    internal class EnumOrderClass
+    {
+        public EnumOrderClass(Gender gender, int order, string name)
+        {
+            Gender = gender;
+            Order = order;
+            Name = name;
+        }
+
+        public Gender Gender { get; set; }
+
+        public int Order { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Gender:{Gender},Order:{Order},Name:{Name}";
+        }
     }
 }
